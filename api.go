be379593d8cac7b441253c8521227f4ad7ad1c9d
Renato Rodrigues de Araujo <renato.r.araujo@gmail.com>
2023-10-09 21:16:07 +0100

    minor: Includes tests and godoc for the options

--- api.go
+++ api.go
@@ -1,3 +1,6 @@
+// Package zenrows provides utility functions to set scraping options
+// for the ZenRows API. These functions help in configuring the request parameters
+// for various scraping features and requirements.
 package zenrows
 
 import (
@@ -6,31 +9,22 @@ import (
 	"strings"
 )
 
-// ScrapeOptions Options to be passed to ZenRows api as query string
+// ScrapeOptions defines functions that modify URL query values
+// based on the chosen scraping options.
 type ScrapeOptions func(values url.Values)
 
-// WithJSRender Render the JavaScript on the page with a headless browser (5 credits/request)
+// WithJSRender enables JavaScript rendering for the scrape request.
+// Consumes 5 credits per request.
 func WithJSRender() ScrapeOptions {
 	return func(values url.Values) {
 		values.Set("js_render", "true")
 	}
 }
 
-// WithJSInstructions appends JavaScript instructions to the URL query parameters.
-// If WithJSRender is not set or set to false, this function will automatically
-// enable it to ensure JavaScript instructions are executed correctly.
+// WithJSInstructions provides JavaScript instructions for the scrape request.
+// It automatically enables WithJSRender to ensure the correct execution of JavaScript instructions.
 //
-// Parameters:
-//   - value: JSON string with JavaScript instructions.
-//
-// Returns:
-//   - ScrapeOptions: Function to modify url.Values with instructions.
-//
-// Example:
-//
-//	opt := WithJSInstructions(`[{"click": ".selector"}, {"wait": 500}]`)
-//	values := url.Values{}
-//	opt(values) // This will also ensure "js_render" is set to "true".
+// value: A JSON string representing the JavaScript instructions.
 func WithJSInstructions(value string) ScrapeOptions {
 	condensed := strings.Join(strings.Fields(value), "")
 	return func(values url.Values) {
@@ -39,21 +33,27 @@ func WithJSInstructions(value string) ScrapeOptions {
 	}
 }
 
-// WithCustomHeaders Enable custom headers to be passed to the request.
+// WithCustomHeaders allows custom headers to be added to the request.
+//
+// value: A boolean indicating if custom headers are to be included.
 func WithCustomHeaders(value bool) ScrapeOptions {
 	return func(values url.Values) {
 		values.Set("custom_headers", strconv.FormatBool(value))
 	}
 }
 
-// WithPremiumProxy Use premium proxies to make the request harder to detect (10-25 credits/request)
+// WithPremiumProxy enables the use of premium proxies for the request.
+// This makes the request less detectable and consumes 10-25 credits per request.
 func WithPremiumProxy() ScrapeOptions {
 	return func(values url.Values) {
 		values.Set("premium_proxy", "true")
 	}
 }
 
-// WithProxyCountry Geolocation of the IP used to make the request. Only for Premium Proxies.
+// WithProxyCountry specifies the geolocation of the IP for the request.
+// Note: Only applicable for Premium Proxies.
+//
+// value: The desired country code for the proxy.
 func WithProxyCountry(value string) ScrapeOptions {
 	return func(values url.Values) {
 		values.Set("premium_proxy", "true")
@@ -61,44 +61,9 @@ func WithProxyCountry(value string) ScrapeOptions {
 	}
 }
 
-// WithSessionID Send a Session ID number to use the same IP for each API Request for up to 10 minutes.
-func WithSessionID(value int) ScrapeOptions {
-	return func(values url.Values) {
-		values.Set("session_id", strconv.Itoa(value))
-	}
-}
-
-// WithDevice Use either desktop or mobile user agents in the headers.
-func WithDevice(value string) ScrapeOptions {
-	return func(values url.Values) {
-		values.Set("device", value)
-	}
-}
-
-// WithOriginalStatus Returns the status code provided by the website.
-func WithOriginalStatus(value bool) ScrapeOptions {
-	return func(values url.Values) {
-		values.Set("original_status", strconv.FormatBool(value))
-	}
-}
-
-// WithWaitFor Wait for a given CSS Selector to load in the DOM before returning the content.
-func WithWaitFor(value string) ScrapeOptions {
-	return func(values url.Values) {
-		values.Set("js_render", "true")
-		values.Set("wait_for", value)
-	}
-}
-
-// WithWait Wait a fixed amount of time before returning the content.
-func WithWait(value int) ScrapeOptions {
-	return func(values url.Values) {
-		values.Set("js_render", "true")
-		values.Set("wait", strconv.Itoa(value))
-	}
-}
-
-// WithBlockResources Block specific resources from loading using this parameter.
+// WithBlockResources prevents specific resources from loading during the scrape request.
+//
+// value: The types of resources to block.
 func WithBlockResources(value string) ScrapeOptions {
 	return func(values url.Values) {
 		values.Set("js_render", "true")
@@ -106,7 +71,10 @@ func WithBlockResources(value string) ScrapeOptions {
 	}
 }
 
-// WithJSONResponse Get content in JSON including XHR or Fetch requests.
+// WithJSONResponse configures the request to return content in JSON format,
+// including any XHR or Fetch requests made.
+//
+// value: A boolean to determine if the response should be in JSON format.
 func WithJSONResponse(value bool) ScrapeOptions {
 	return func(values url.Values) {
 		values.Set("js_render", "true")
@@ -114,7 +82,9 @@ func WithJSONResponse(value bool) ScrapeOptions {
 	}
 }
 
-// WithWindowWidth Set browser's window width.
+// WithWindowWidth defines the browser window width for the request.
+//
+// value: The desired window width in pixels.
 func WithWindowWidth(value int) ScrapeOptions {
 	return func(values url.Values) {
 		values.Set("js_render", "true")
@@ -122,7 +92,9 @@ func WithWindowWidth(value int) ScrapeOptions {
 	}
 }
 
-// WithWindowHeight Set browser's window height.
+// WithWindowHeight defines the browser window height for the request.
+//
+// value: The desired window height in pixels.
 func WithWindowHeight(value int) ScrapeOptions {
 	return func(values url.Values) {
 		values.Set("js_render", "true")
@@ -130,21 +102,29 @@ func WithWindowHeight(value int) ScrapeOptions {
 	}
 }
 
-// WithCSSExtractor Define CSS Selectors to extract data from the HTML.
+// WithCSSExtractor sets CSS Selectors to extract specific data from the HTML.
+//
+// value: The desired CSS selectors.
 func WithCSSExtractor(value string) ScrapeOptions {
 	return func(values url.Values) {
 		values.Set("css_extractor", value)
 	}
 }
 
-// WithAutoparse Use our auto parser algorithm to automatically extract data.
+// WithAutoparse employs the auto-parser algorithm for the request,
+// which extracts data from the page automatically.
+//
+// value: A boolean to determine if the auto parser should be used.
 func WithAutoparse(value bool) ScrapeOptions {
 	return func(values url.Values) {
 		values.Set("autoparse", strconv.FormatBool(value))
 	}
 }
 
-// WithResolveCaptcha Use a CAPTCHA solver integration to automatically solve interactive CAPTCHAs in the page.
+// WithResolveCaptcha integrates a CAPTCHA solver for the request,
+// enabling automatic solving of CAPTCHAs on the page.
+//
+// value: A boolean to determine if the CAPTCHA solver should be used.
 func WithResolveCaptcha(value bool) ScrapeOptions {
 	return func(values url.Values) {
 		values.Set("js_render", "true")
@@ -152,7 +132,59 @@ func WithResolveCaptcha(value bool) ScrapeOptions {
 	}
 }
 
-// ApplyParameters Applies the api parameters to the query string
+// WithDevice sets the user agent type (either desktop or mobile) for the request.
+//
+// value: A string specifying the device type ("desktop" or "mobile").
+func WithDevice(value string) ScrapeOptions {
+	return func(values url.Values) {
+		values.Set("device", value)
+	}
+}
+
+// WithOriginalStatus configures the request to return the status code as provided by the website.
+//
+// value: A boolean determining if the original status code should be returned.
+func WithOriginalStatus(value bool) ScrapeOptions {
+	return func(values url.Values) {
+		values.Set("original_status", strconv.FormatBool(value))
+	}
+}
+
+// WithWaitFor delays the request until a specific CSS Selector is loaded in the DOM.
+//
+// value: A string specifying the CSS Selector to wait for.
+func WithWaitFor(value string) ScrapeOptions {
+	return func(values url.Values) {
+		values.Set("js_render", "true")
+		values.Set("wait_for", value)
+	}
+}
+
+// WithWait introduces a fixed delay before the content is returned.
+//
+// value: An integer specifying the wait time in milliseconds.
+func WithWait(value int) ScrapeOptions {
+	return func(values url.Values) {
+		values.Set("js_render", "true")
+		values.Set("wait", strconv.Itoa(value))
+	}
+}
+
+// WithSessionID sets the Session ID number for the scrape request.
+// This allows the use of the same IP for each API Request for up to 10 minutes.
+//
+// sessionID: An integer representing the Session ID.
+func WithSessionID(sessionID int) ScrapeOptions {
+	return func(values url.Values) {
+		values.Set("session_id", strconv.Itoa(sessionID))
+	}
+}
+
+// ApplyParameters applies the chosen scraping options to a URL.
+// It modifies the URL's query string based on the provided scraping options.
+//
+// u: The target URL.
+// params: The ScrapeOptions to be applied to the URL.
 func ApplyParameters(u *url.URL, params ...ScrapeOptions) *url.URL {
 	values := u.Query()
 	for _, param := range params {
---
+++ api_test.go
@@ -0,0 +1,183 @@
+package zenrows_test
+
+import (
+	"github.com/stretchr/testify/require"
+	"net/url"
+	"testing"
+
+	"github.com/renatoaraujo/go-zenrows"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestScrapeOptions(t *testing.T) {
+	tests := []struct {
+		name     string
+		option   zenrows.ScrapeOptions
+		expected url.Values
+	}{
+		{
+			"WithJSRender",
+			zenrows.WithJSRender(),
+			url.Values{"js_render": []string{"true"}},
+		},
+		{
+			"WithJSInstructions",
+			zenrows.WithJSInstructions(`[{"click": ".selector"}, {"wait": 500}]`),
+			url.Values{"js_render": []string{"true"}, "js_instructions": []string{`[{"click":".selector"},{"wait":500}]`}},
+		},
+		{
+			"WithCustomHeaders",
+			zenrows.WithCustomHeaders(true),
+			url.Values{"custom_headers": []string{"true"}},
+		},
+		{
+			"WithPremiumProxy",
+			zenrows.WithPremiumProxy(),
+			url.Values{"premium_proxy": []string{"true"}},
+		},
+		{
+			"WithProxyCountry",
+			zenrows.WithProxyCountry("US"),
+			url.Values{"premium_proxy": []string{"true"}, "proxy_country": []string{"US"}},
+		},
+		{
+			"WithBlockResources",
+			zenrows.WithBlockResources("image"),
+			url.Values{"js_render": []string{"true"}, "block_resources": []string{"image"}},
+		},
+		{
+			"WithJSONResponse",
+			zenrows.WithJSONResponse(true),
+			url.Values{"js_render": []string{"true"}, "json_response": []string{"true"}},
+		},
+		{
+			"WithWindowWidth",
+			zenrows.WithWindowWidth(1920),
+			url.Values{"js_render": []string{"true"}, "window_width": []string{"1920"}},
+		},
+		{
+			"WithWindowHeight",
+			zenrows.WithWindowHeight(1080),
+			url.Values{"js_render": []string{"true"}, "window_height": []string{"1080"}},
+		},
+		{
+			"WithCSSExtractor",
+			zenrows.WithCSSExtractor(".content"),
+			url.Values{"css_extractor": []string{".content"}},
+		},
+		{
+			"WithAutoparse",
+			zenrows.WithAutoparse(true),
+			url.Values{"autoparse": []string{"true"}},
+		},
+		{
+			"WithResolveCaptcha",
+			zenrows.WithResolveCaptcha(true),
+			url.Values{"js_render": []string{"true"}, "resolve_captcha": []string{"true"}},
+		},
+		{
+			"WithDevice",
+			zenrows.WithDevice("desktop"),
+			url.Values{"device": []string{"desktop"}},
+		},
+		{
+			"WithOriginalStatus",
+			zenrows.WithOriginalStatus(true),
+			url.Values{"original_status": []string{"true"}},
+		},
+		{
+			"WithWaitFor",
+			zenrows.WithWaitFor(".selector"),
+			url.Values{"js_render": []string{"true"}, "wait_for": []string{".selector"}},
+		},
+		{
+			"WithWait",
+			zenrows.WithWait(500),
+			url.Values{"js_render": []string{"true"}, "wait": []string{"500"}},
+		},
+		{
+			"WithSessionID",
+			zenrows.WithSessionID(12345),
+			url.Values{"session_id": []string{"12345"}},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			values := url.Values{}
+			tt.option(values)
+			assert.Equal(t, tt.expected, values)
+		})
+	}
+}
+
+func TestApplyParameters(t *testing.T) {
+	tests := []struct {
+		name     string
+		options  []zenrows.ScrapeOptions
+		expected url.Values
+	}{
+		{
+			"Single Option - WithJSRender",
+			[]zenrows.ScrapeOptions{zenrows.WithJSRender()},
+			url.Values{"js_render": []string{"true"}},
+		},
+		{
+			"Multiple Options - WithJSInstructions and WithCustomHeaders",
+			[]zenrows.ScrapeOptions{
+				zenrows.WithJSInstructions(`[{"click": ".selector"}, {"wait": 500}]`),
+				zenrows.WithCustomHeaders(true),
+			},
+			url.Values{"js_render": []string{"true"}, "js_instructions": []string{`[{"click":".selector"},{"wait":500}]`}, "custom_headers": []string{"true"}},
+		},
+		{
+			"Multiple Options - WithPremiumProxy and WithProxyCountry",
+			[]zenrows.ScrapeOptions{
+				zenrows.WithPremiumProxy(),
+				zenrows.WithProxyCountry("US"),
+			},
+			url.Values{"premium_proxy": []string{"true"}, "proxy_country": []string{"US"}},
+		},
+		{
+			"Multiple Options - WithBlockResources and WithJSONResponse",
+			[]zenrows.ScrapeOptions{
+				zenrows.WithBlockResources("image"),
+				zenrows.WithJSONResponse(true),
+			},
+			url.Values{"js_render": []string{"true"}, "block_resources": []string{"image"}, "json_response": []string{"true"}},
+		},
+		{
+			"Multiple Options - WithWindowWidth and WithWindowHeight",
+			[]zenrows.ScrapeOptions{
+				zenrows.WithWindowWidth(1920),
+				zenrows.WithWindowHeight(1080),
+			},
+			url.Values{"js_render": []string{"true"}, "window_width": []string{"1920"}, "window_height": []string{"1080"}},
+		},
+		{
+			"Multiple Options - WithCSSExtractor and WithAutoparse",
+			[]zenrows.ScrapeOptions{
+				zenrows.WithCSSExtractor(".content"),
+				zenrows.WithAutoparse(true),
+			},
+			url.Values{"autoparse": []string{"true"}, "css_extractor": []string{".content"}},
+		},
+		{
+			"Multiple Options - WithResolveCaptcha and WithJSInstructions",
+			[]zenrows.ScrapeOptions{
+				zenrows.WithResolveCaptcha(true),
+				zenrows.WithJSInstructions(`[{"click": ".button"}]`),
+			},
+			url.Values{"js_render": []string{"true"}, "js_instructions": []string{`[{"click":".button"}]`}, "resolve_captcha": []string{"true"}},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			baseURL, err := url.Parse("https://example.com")
+			require.NoError(t, err)
+			resultURL := zenrows.ApplyParameters(baseURL, tt.options...)
+			assert.Equal(t, tt.expected, resultURL.Query())
+		})
+	}
+}
